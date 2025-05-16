19735e3f095bfd0086d763d85865ec1410ce76f4
Bhavesh-Mankar51 <bhaveshmankar770@gmail.com>
2025-05-16 16:00:56 +0530

    First Commit

---
+++ src/launchbase/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from visits.models import PageVisit
+
+def home_page_view(request):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path = request.path)
+    my_title = "Welcome to LaunchBase"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "total_visit_count": qs.count(),
+        "percent": (page_qs.count() / qs.count()) * 100 if qs.count() > 0 else 0,
+    }
+    
+    PageVisit.objects.create(path = request.path)
+    return render(request, 'home.html', my_context)
